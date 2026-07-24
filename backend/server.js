194b4624c88a9e52b63f409f0750da2c07e97dee
Dulshan-Rajeewa <dulshanrajeewa@gmail.com>
2026-07-25 00:16:19 +0530

    Versel deployment changes

--- backend/server.js
+++ backend/server.js
@@ -174,7 +174,10 @@ app.post("/api/analyze", upload.single("prescription"), async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () =>
-  console.log(`RxGuard Orchestrator is running on port ${PORT}`),
-);
+
+module.exports = app;
+
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () =>
+//   console.log(`RxGuard Orchestrator is running on port ${PORT}`),
+// );
